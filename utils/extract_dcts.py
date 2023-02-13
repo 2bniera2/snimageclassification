cc25773d02e2062f8356e9af122b968c238b1cba
Raumaan <87394739+arrrayyy@users.noreply.github.com>
2023-02-13 15:20:01 +0000

    Update extract_dcts.py
    Figured out that the DCT coefficient list is stored in row major order so the transpose was stupid

--- utils/extract_dcts.py
+++ utils/extract_dcts.py
@@ -10,16 +10,16 @@ import h5py
 def histogram_builder(dct, sf_range, his_range):
     bin_num = len(range(his_range[0], his_range[1])) + 1
     # indexes to all AC coefficients
-    # indexes = [
-    #     (0, 1), (1, 0), (2, 0), (1, 1), (0, 2), (0, 3), (1, 2), (2, 1), (3, 0), (4, 0), (3, 1), (2, 2), (1, 3),
-    #     (0, 4), (0, 5), (1, 4), (2, 3), (3, 2), (4, 1), (5, 0), (6, 0), (5, 1), (4, 2), (3, 3), (2, 4), (1, 5),
-    #     (0, 6), (0, 7), (1, 6), (2, 5), (3, 4), (4, 3), (5, 2), (6, 1), (7, 0), (7, 1), (6, 2), (5, 3), (4, 4),
-    #     (3, 5), (2, 6), (1, 7), (2, 7), (3, 6), (4, 5), (5, 4), (6, 3), (7, 2), (7, 3), (6, 4), (5, 5), (4, 6),
-    #     (3, 7), (4, 7), (5, 6), (6, 5), (7, 4), (7, 5), (6, 6), (5, 7), (6, 7), (7, 6), (7, 7)
-    # ]
+    indexes = [
+        (0, 1), (1, 0), (2, 0), (1, 1), (0, 2), (0, 3), (1, 2), (2, 1), (3, 0), (4, 0), (3, 1), (2, 2), (1, 3),
+        (0, 4), (0, 5), (1, 4), (2, 3), (3, 2), (4, 1), (5, 0), (6, 0), (5, 1), (4, 2), (3, 3), (2, 4), (1, 5),
+        (0, 6), (0, 7), (1, 6), (2, 5), (3, 4), (4, 3), (5, 2), (6, 1), (7, 0), (7, 1), (6, 2), (5, 3), (4, 4),
+        (3, 5), (2, 6), (1, 7), (2, 7), (3, 6), (4, 5), (5, 4), (6, 3), (7, 2), (7, 3), (6, 4), (5, 5), (4, 6),
+        (3, 7), (4, 7), (5, 6), (6, 5), (7, 4), (7, 5), (6, 6), (5, 7), (6, 7), (7, 6), (7, 7)
+    ]
 
     # # obtain spatial frequencies
-    # coords = indexes[0: sf_range]
+    coords = indexes[0: sf_range]
     # build histogram
     his = np.zeros((sf_range,bin_num))
 
@@ -29,7 +29,7 @@ def histogram_builder(dct, sf_range, his_range):
     # iterate over each 8x8 block in a patch and build up histogram
     for x in range(c_H):
         for y in range(c_W):
-            sf = np.array([dct[x][y][i] for i in range(sf_range)])
+            sf = np.array([dct[x][y].reshape((8, 8))[c[0]][c[1]] for c in coords])
 
             for i, f in enumerate(sf):
                 h, b = np.histogram(f, bins=bin_num, range=(his_range[0], his_range[1]))
